f4ab350cd26c63edfd62a121498a388b583d5ce3
viviaan09 <ngvivian682@gmail.com>
2025-05-13 13:38:52 +0700

    mengedit view layout

--- LATIHAN/resources/views/layout/footer.blade.php
+++ LATIHAN/resources/views/layout/footer.blade.php
@@ -1,3 +1,14 @@
-<div>
-    <!-- Let all your things have their places; let each part of your business have its time. - Benjamin Franklin -->
-</div>
+<!--begin::Footer-->
+<footer class="app-footer">
+        <!--begin::To the end-->
+        <div class="float-end d-none d-sm-inline">Anything you want</div>
+        <!--end::To the end-->
+        <!--begin::Copyright-->
+        <strong>
+          Copyright &copy; 2014-2024&nbsp;
+          <a href="https://adminlte.io" class="text-decoration-none">AdminLTE.io</a>.
+        </strong>
+        All rights reserved.
+        <!--end::Copyright-->
+      </footer>
+      <!--end::Footer-->
